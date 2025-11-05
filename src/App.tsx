190450e2fe344321e0aa539ababc10d32e9c52ab
YismelR <yismel_rodriguez@outlook.com>
2025-11-05 13:35:06 -0500

    changed the unitsswitch to a dropdown button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Body from "./components/Body";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="my-12 mx-24">
       <Header />
-    </>
+      <Body />
+    </div>
   );
 }
 
