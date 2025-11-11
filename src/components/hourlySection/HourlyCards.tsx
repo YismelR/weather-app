538b6e08846ccd39bf2e45e1ac4ad4a157ca90b6
Yismel Rodriguez <yismel_rodriguez@outlook.com>
2025-11-11 15:32:26 -0500

    created a data folder to store the data coming in later and readability

--- src/components/hourlySection/HourlyCards.tsx
+++ src/components/hourlySection/HourlyCards.tsx
@@ -1,22 +1,27 @@
-import cloudy from "../../assets/images/icon-partly-cloudy.webp";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { hourlyCards } from "@/data/hourlyCards";
 
 export default function HourlyCards() {
   return (
     <ScrollArea className="h-11/12 w-full  ">
       <div className="flex flex-col gap-4 overflow-hidden">
-        <div className="flex place-items-center justify-between bg-Neutral-700 py-2 px-3 rounded-lg border border-Neutral-600">
-          <div className="flex place-items-center">
-            <img
-              src={cloudy}
-              className="size-8
-            "
-            />
-            <p className="text-sm">3 PM</p>
+        {hourlyCards.map((hourly, idx) => (
+          <div
+            key={idx}
+            className="flex place-items-center justify-between bg-Neutral-700 py-2 px-3 rounded-lg border border-Neutral-600"
+          >
+            <div className="flex place-items-center">
+              <img
+                src={hourly.image.url}
+                className="size-8
+              "
+              />
+              <p className="text-sm">{hourly.time} PM</p>
+            </div>
+            <p className="text-sm">{hourly.temperature}°</p>
           </div>
-          <p className="text-sm">68°</p>
-        </div>
+        ))}
       </div>
     </ScrollArea>
   );
---
+++ src/data/hourlyCards.tsx
@@ -0,0 +1,19 @@
+import cloudy from "../assets/images/icon-partly-cloudy.webp";
+
+export const hourlyCards = [
+  {
+    image: { url: cloudy },
+    time: 3,
+    temperature: 68,
+  },
+  {
+    image: { url: cloudy },
+    time: 3,
+    temperature: 68,
+  },
+  {
+    image: { url: cloudy },
+    time: 3,
+    temperature: 68,
+  },
+];
