269bc6cde1bdad8a3a36c1034079b5a2b47831f3
Yismel Rodriguez <yismel_rodriguez@outlook.com>
2025-11-18 16:41:39 -0500

    forgot to fix the body component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex flex-col gap-8 my-12 px-12 min-w-5xl">
+    <div className="flex flex-col lg:gap-8 lg:my-12 lg:px-12 lg:min-w-5xl">
       <Header />
       <Body />
     </div>
