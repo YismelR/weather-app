7cad5bc250cd0c72f7d3f36f97101adcc28aa481
Yismel Rodriguez <yismel_rodriguez@outlook.com>
2025-11-18 21:20:08 -0500

    navigation responsive layout is done

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,16 @@
 import iconSearch from "../assets/images/icon-search.svg";
 export default function Navigation() {
   return (
-    <form className="flex lg:gap-4">
-      <div className="flex lg:gap-2 lg:px-4 lg:py-2 lg:min-w-96 rounded-lg bg-card-bg">
-        <img src={iconSearch} className="lg:size-4" />
+    <form className="flex flex-col md:flex-row gap-2 w-full md:w-fit">
+      <div className="flex gap-2 px-4 py-2 md:min-w-96 rounded-lg bg-card-bg">
+        <img src={iconSearch} className="size-4" />
         <input
           type="text"
           placeholder="Search for a place..."
           name="search"
         ></input>
       </div>
-      <button className="bg-Blue-500 lg:px-4 rounded-lg" type="submit">
+      <button className="bg-Blue-500 p-2 md:px-4 rounded-lg" type="submit">
         Search
       </button>
     </form>
