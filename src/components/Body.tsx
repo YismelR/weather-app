d19156f8e5e51a45499994d7c52917507847d9bd
Yismel Rodriguez <yismel_rodriguez@outlook.com>
2025-11-18 20:35:14 -0500

    done with the title section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,8 +3,10 @@ import WeatherDisplay from "./WeatherDisplay";
 
 export default function Body() {
   return (
-    <section className="flex flex-col place-items-center lg:gap-10">
-      <h1 className="lg:text-4xl">How's the sky looking today?</h1>
+    <section className="flex flex-col place-items-center gap-10">
+      <h1 className="text-4xl text-center tracking-wide px-10 min-[375px]:px-14 min-[425px]:px-20 md:px-0">
+        How's the sky looking today?
+      </h1>
       <Navigation />
       {/* <WeatherDisplay /> */}
     </section>
