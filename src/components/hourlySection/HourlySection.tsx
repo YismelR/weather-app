928556bed3a71c4305aecd688188b4eeb49f10f8
Yismel Rodriguez <yismel_rodriguez@outlook.com>
2025-11-26 16:13:24 -0500

    fixed the dailyforecast and hourly section for the medium size screen(tablet)

--- src/components/hourlySection/HourlySection.tsx
+++ src/components/hourlySection/HourlySection.tsx
@@ -3,7 +3,7 @@ import HourlyCards from "./HourlyCards";
 
 export default function HourlySection() {
   return (
-    <section className="flex flex-col mb-10 md:mb-0 p-3 gap-3 md:gap-4 bg-card-bg md:p-4 rounded-xl max-h-[495px] md:max-h-[530px] ">
+    <section className="flex flex-col mb-10 md:mb-0 p-3 gap-3 md:gap-4 bg-card-bg md:p-4 rounded-xl max-h-[495px] md:max-h-[560px] lg:max-h-[530px] ">
       <div className="flex justify-between place-items-center">
         <p className="font-semibold">Hourly forecast</p>
         <DayDropdown />
